980f9cf64c49d72d35177644155eccbb951aeeea
yankeeBrit <nawebb79@gmail.com>
2026-01-13 23:13:59 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Scoreboard from './Scoreboard';
-
-ReactDOM.render(<Scoreboard source="json/mlb.json" />, document.getElementById('scoreboard'));
